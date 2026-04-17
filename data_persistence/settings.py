6ce6a112fee3859d0ca896211dc3310b9aa12dbe
Atolagbe folake racheal <atolagbefolakeracheal@gmail.com>
2026-04-17 17:12:09 +0100

    updated

--- data_persistence/settings.py
+++ data_persistence/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
